a700a3438bcb5a43ad19baff8bd38e0031a45735
DIYA-SHEHEN <160301250+DIYA-SHEHEN@users.noreply.github.com>
2024-02-19 17:17:53 +0530

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ques extends StatefulWidget {
+  const Ques({Key? key}) : super(key: key);
+
+  @override
+  State<Ques> createState() => _QuesState();
+}
+
+class _QuesState extends State<Ques> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.black,);
+  }
+}
